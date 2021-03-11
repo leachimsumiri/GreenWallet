2efb1b2a8ec007ae862a7c337c64008741a73698
Stefan Schmitzhofer <stefan.schmitzhofer@gmail.com>
2021-03-11 01:19:08 +0100

    Added minor python formatting error

--- api/main.py
+++ api/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, json
 
-transactions = [{ greenIndex: -42, targetName: "OMV Stinkstraße 32", IBAN:"AT123453453452234", amount: "2 Millionen Euro", purpose: "", buchungsdatum: "heute" }, { greenIndex: "over 9000", targetName: "Green Company: Pflanze zwei Bäume", IBAN: "AT89498239823", amount: "60 cent", purpose: "Ich liebe die Natur!!!", buchungsdatum: "gestern" }]
+transactions = '[{ greenIndex: -42, targetName: "OMV Stinkstraße 32", IBAN:"AT123453453452234", amount: "2 Millionen Euro", purpose: "", buchungsdatum: "heute" }, { greenIndex: "over 9000", targetName: "Green Company: Pflanze zwei Bäume", IBAN: "AT89498239823", amount: "60 cent", purpose: "Ich liebe die Natur!!!", buchungsdatum: "gestern" }]'
 
 api = Flask(__name__)
 
