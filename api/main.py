5fdb741ff989ce79aa197b9a3a976dce59502a9b
Stefan Schmitzhofer <stefan.schmitzhofer@gmail.com>
2021-03-12 12:22:48 +0100

    Dumping API at start, several setup tests

--- api/main.py
+++ api/main.py
@@ -7,19 +7,23 @@ from obp.lib import obp
 
 #@Stefan die Transaktionen kannst mal verwenden
 obpTransactions = obp.getPublicTransactions
+mockTransactions = '[{ greenIndex: -42, targetName: "OMV Stinkstraße 32", IBAN:"AT123453453452234", amount: "2 Millionen Euro", purpose: "", buchungsdatum: "heute" }, { greenIndex: "over 9000", targetName: "Green Company: Pflanze zwei Bäume", IBAN: "AT89498239823", amount: "60 cent", purpose: "Ich liebe die Natur!!!", buchungsdatum: "gestern" }]'
 
-transactions = '[{ greenIndex: -42, targetName: "OMV Stinkstraße 32", IBAN:"AT123453453452234", amount: "2 Millionen Euro", purpose: "", buchungsdatum: "heute" }, { greenIndex: "over 9000", targetName: "Green Company: Pflanze zwei Bäume", IBAN: "AT89498239823", amount: "60 cent", purpose: "Ich liebe die Natur!!!", buchungsdatum: "gestern" }]'
+greenIndex = {
+    'omv':                      -42,
+    'green company':            9001,
+}
 
 api = Flask(__name__)
-
 @api.route('/getGreenTransactions', methods=['GET'])
 # @cross_origin()
 def get_transactions():
-    response = jsonify(transactions)
+    response = jsonify(mockTransactions)
     # Enable Access-Control-Allow-Origin
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
     # return json.dumps(transactions)
 
 if __name__ == '__main__':
+    print(json.dumps(obpTransactions(), indent=4, sort_keys=True))
     api.run()
